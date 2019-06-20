a9650d80dc054778cecf41a901d65c1dbdd5f33a
Nikita Vinokurov <43025259+aviator737x@users.noreply.github.com>
2019-06-20 20:07:39 +0300

    Update Test_CLI.py

--- CLI/Test_CLI.py
+++ CLI/Test_CLI.py
@@ -22,6 +22,9 @@ class CLITest(unittest.TestCase):
         
     def test_pwd1(self):
         assert (pwd() == os.getcwd())
+        
+    def test_wc(self):
+        assert (wc(2, ["test.txt"]) == "2 2 15\n")
 
 
 if __name__ == '__main__':
